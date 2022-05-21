139b3616f3beffe4346a779a17200b0b626ec91c
RESN036 <lhaisferreirada@gmail.com>
2022-05-21 19:06:50 -0300

    codeficador e decodificador construido

---
+++ cifra.js
@@ -0,0 +1,59 @@
+function cifrar(mensagem,chave) {
+    let mensagemArray = mensagem.split('')
+    let mesagemCodificada = []
+    let arrayCodificador = []
+
+    for (let i = 0; i < mensagemArray.length; i++) {
+        if (mensagemArray[i].charCodeAt() >= 65 && mensagemArray[i].charCodeAt() <= 90){
+            let valor = ((mensagemArray[i].charCodeAt()) - 65 + chave) % 26
+            arrayCodificador.push(valor + 65)
+        }
+        else if (mensagemArray[i].charCodeAt() >= 97 && mensagemArray[i].charCodeAt() <= 122){
+            let valor = ((mensagemArray[i].charCodeAt()) - 97 + chave) % 26
+            arrayCodificador.push(valor + 65)
+        }
+        else{
+            arrayCodificador.push(mesagemCodificada[i].charCodeAt())
+        }
+    }
+    for (let i = 0; i < arrayCodificador.length; i++) {
+        let element = arrayCodificador[i];
+        mesagemCodificada.push(element)
+    }
+    return mesagemCodificada.join('')
+}
+
+function decifrar(mensagem,chave) {
+    let mensagemArray = mensagem.split('')
+    let mesagemCodificada = []
+    let arrayCodificador = []
+
+    for (let i = 0; i < mensagemArray.length; i++) {
+        if (mensagemArray[i].charCodeAt() >= 65 && mensagemArray[i].charCodeAt() <= 90){
+            let valor = ((mensagemArray[i].charCodeAt()) - 65 - chave) % 26
+            arrayCodificador.push((valor < 0 ? valor + 26: valor)+ 65)
+        }
+        else if (mensagemArray[i].charCodeAt() >= 97 && mensagemArray[i].charCodeAt() <= 122){
+            let valor = ((mensagemArray[i].charCodeAt()) - 97 - chave) % 26
+            arrayCodificador.push((valor < 0 ? valor + 26: valor)+ 97)
+        }
+        else{
+            arrayCodificador.push(mesagemCodificada[i].charCodeAt())
+        }
+    }
+    for (let i = 0; i < arrayCodificador.length; i++) {
+        let element = arrayCodificador[i];
+        mesagemCodificada.push(element)
+    }
+    return mesagemCodificada.join('')
+}
+
+function base64Codifica(mensagem){
+    let mensagemCodificada = btoa(mensagem)
+    return mensagemCodificada
+}
+
+function base64Decodifica(mensagem){
+    let mensagemDecodificada = atob(mensagem)
+    return mensagemDecodificada
+}
